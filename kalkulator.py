80f8f96d95e67fc9699af7415acf4b5f8bfb0eda
Kacper <kkwasnioch@gmail.com>
2021-03-25 13:54:19 +0100

    dodane wyswietlanie

--- kalkulator.py
+++ kalkulator.py
@@ -5,8 +5,11 @@ def dodawanie(a,b):
 def odejmowanie(a,b):
     c = a-b
     return c
-    
+def wyswietl():
+    print("Witaj!")
+   
 a = int(input())
 b = int(input())
+wyswietl()
 dodawanie(a,b)
 odejmowanie(a,b)
